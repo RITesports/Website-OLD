017ec6efe4e1e0cf8824bd23d7c6a14ee9e077c3
Ryan Gillie <rag4214@g.rit.edu>
2020-06-03 20:02:04 -0400

    Theme (#3)
    * basic color theme
    
    * wrap app with ThemeProvider and custom theme

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import './index.scss';
 
 import App from './App';
+import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
