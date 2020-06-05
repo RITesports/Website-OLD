dc8d96add3eb2bad4c0a2d2bbd5316c7caa0b87b
Zach Cook <zjc9022@g.rit.edu>
2020-06-05 18:54:11 -0400

    Code style fixes

--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
-import Menu from 'mdi-material-ui/Menu';
+import MenuIcon from 'mdi-material-ui/Menu';
+
 import MenuDrawer from './MenuDrawer';
 import { RITEsports_Wordmark_LongWhite } from "../../assets/images";
 
@@ -15,8 +16,8 @@ const Header: React.FC = () => {
     <header>
       <AppBar color="secondary">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={toggleDrawer}>
-            <Menu />
+          <IconButton edge="start" color="inherit" aria-label="open menu drawer" onClick={toggleDrawer}>
+            <MenuIcon />
           </IconButton>
           <img src={RITEsports_Wordmark_LongWhite} alt="RIT Esports Wordmark Long White" style={{ width: "20vh" }} />
         </Toolbar>
