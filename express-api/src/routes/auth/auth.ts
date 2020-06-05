82f8f8912ed97286033abe41d14d1bbc2bb2dc7e
Ryan Gillie <rag4214@g.rit.edu>
2020-06-05 18:46:20 -0400

    isLoggedIn and logout routes

--- express-api/src/routes/auth/auth.ts
+++ express-api/src/routes/auth/auth.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 import googleRoutes from './google';
+import jwtConfig, { JWT } from '../../configs/jwt';
 
 const router = Router();
 
 router.use('/google', googleRoutes);
 
-router.get('/', (req, res) => res.status(200).json({
+router.get('/isLoggedIn', (req, res) => res.status(200).json({
   status: 200,
-  message: 'Auth Routes',
+  isLoggedIn: !!req.cookies[jwtConfig.cookieName],
+  user: req.cookies[jwtConfig.cookieName] && (jwt.decode(req.cookies[jwtConfig.cookieName]) as JWT).user,
 }));
+
+router.get('/logout', (req, res) => {
+  res.clearCookie(jwtConfig.cookieName);
+  res.redirect('/');
+});
 
 export default router;
