851e65d4250173f058e102a350bda13837b7e33b
Ryan Gillie <rag4214@g.rit.edu>
2020-06-06 15:13:52 -0400

    jwt config readability

--- express-api/src/configs/jwt/jwt.ts
+++ express-api/src/configs/jwt/jwt.ts
@@ -4,6 +4,10 @@ import { SignOptions } from 'jsonwebtoken';
 
 import { User } from '../../models/user';
 
+export interface JWT {
+  user: User;
+}
+
 const hours = 0;
 const minutes = 30;
 const seconds = 0;
@@ -11,7 +15,6 @@ const seconds = 0;
 const maxAge = (hours * 3600000) + (minutes * 60000) + (seconds * 1000);
 const expiresIn = (hours * 3600) + (minutes * 60) + seconds;
 
-const cookieName = process.env.NODE_ENV !== 'development' ? '__Secure-jwt' : 'jwt';
 const cookieOptions: CookieOptions = {
   secure: process.env.NODE_ENV !== 'development',
   httpOnly: process.env.NODE_ENV !== 'development',
@@ -19,19 +22,18 @@ const cookieOptions: CookieOptions = {
   maxAge,
 };
 
-const tokenOptions: SignOptions = { algorithm: 'RS256', expiresIn };
+const tokenOptions: SignOptions = {
+  algorithm: 'RS256',
+  expiresIn,
+};
 
 const jwtConfig = {
   publicKey: fs.readFileSync('jwt_public.pem'),
   privateKey: fs.readFileSync('jwt_private.pem'),
 
-  cookieName,
+  cookieName: process.env.NODE_ENV !== 'development' ? '__Secure-jwt' : 'jwt',
   cookieOptions,
   tokenOptions,
 };
-
-export interface JWT {
-  user: User;
-}
 
 export default jwtConfig;
