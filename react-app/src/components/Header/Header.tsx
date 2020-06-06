006cd4528cf3fb47cf1fa6bc276a1ba2a8b73250
Zach Cook <47161288+zjc9022@users.noreply.github.com>
2020-06-06 15:43:49 -0400

    Header (#15)
    * Added basic AppBar header and a temporary drawer
    
    * Added wordmark to header
    
    * Moved to mdi-material-ui package for icons. Added socials to drawer.
    
    * Added links to all in drawer.
    
    * Added Divider to the drawer under socials.
    
    * Changed shopping cart icons to shirt icons in drawer.
    
    * Code style fixes
    
    * Linted.
    
    * Changed from style prop to makeStyles on Header.
    
    * Code style fix.

--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from 'mdi-material-ui/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Header: React.FC = () => <header />;
+import MenuDrawer from './MenuDrawer';
+import { RITEsportsWordmarkLongWhite } from '../../assets/images';
+
+const useStyles = makeStyles({
+  wordMark: {
+    width: '20vh',
+  },
+});
+
+const Header: React.FC = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => setOpen((pOpen) => !pOpen);
+
+  return (
+    <header>
+      <AppBar color="secondary">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open menu drawer" onClick={toggleDrawer}>
+            <MenuIcon />
+          </IconButton>
+          <img src={RITEsportsWordmarkLongWhite} alt="RIT Esports Wordmark Long White" className={classes.wordMark} />
+        </Toolbar>
+      </AppBar>
+      <MenuDrawer open={open} onClose={toggleDrawer} />
+    </header>
+  );
+};
 
 export default Header;
---
+++ react-app/src/components/Header/MenuDrawer/MenuDrawer.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Link as RouterLink } from 'react-router-dom';
+import Divider from '@material-ui/core/Divider';
+import TshirtCrew from 'mdi-material-ui/TshirtCrew';
+import GamepadSquare from 'mdi-material-ui/GamepadSquare';
+import HomeVariant from 'mdi-material-ui/HomeVariant';
+import Facebook from 'mdi-material-ui/Facebook';
+import Twitter from 'mdi-material-ui/Twitter';
+import Instagram from 'mdi-material-ui/Instagram';
+import Twitch from 'mdi-material-ui/Twitch';
+import Discord from 'mdi-material-ui/Discord';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+const MenuDrawer: React.FC<Props> = ({ open, onClose }) => (
+  <Drawer open={open} onClose={onClose}>
+    <List>
+      <ListItem button component={RouterLink} to="/">
+        <ListItemIcon><HomeVariant /></ListItemIcon>
+        <ListItemText>Home</ListItemText>
+      </ListItem>
+      <ListItem button component={RouterLink} to="/teams">
+        <ListItemIcon><GamepadSquare /></ListItemIcon>
+        <ListItemText>Teams</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem button component="a" href="https://metathreads.com/collections/rit-esports">
+        <ListItemIcon><TshirtCrew /></ListItemIcon>
+        <ListItemText>Sports Wear</ListItemText>
+      </ListItem>
+      <ListItem button component="a" href="https://teamstore.gtmsportswear.com/ritesports">
+        <ListItemIcon><TshirtCrew /></ListItemIcon>
+        <ListItemText>Casual Wear</ListItemText>
+      </ListItem>
+      <Divider />
+      <ListItem button component="a" href="https://www.facebook.com/esportsrit">
+        <ListItemIcon><Facebook /></ListItemIcon>
+        <ListItemText>Facebook</ListItemText>
+      </ListItem>
+      <ListItem button component="a" href="https://twitter.com/RITesports">
+        <ListItemIcon><Twitter /></ListItemIcon>
+        <ListItemText>Twitter</ListItemText>
+      </ListItem>
+      <ListItem button component="a" href="https://www.instagram.com/ritesports">
+        <ListItemIcon><Instagram /></ListItemIcon>
+        <ListItemText>Instagram</ListItemText>
+      </ListItem>
+      <ListItem button component="a" href="https://www.twitch.tv/ritesports">
+        <ListItemIcon><Twitch /></ListItemIcon>
+        <ListItemText>Twitch</ListItemText>
+      </ListItem>
+      <ListItem button component="a" href="https://discordapp.com/invite/ritesports">
+        <ListItemIcon><Discord /></ListItemIcon>
+        <ListItemText>Discord</ListItemText>
+      </ListItem>
+      <Divider />
+    </List>
+  </Drawer>
+);
+
+export default MenuDrawer;
