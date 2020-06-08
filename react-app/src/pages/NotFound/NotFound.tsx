6a1007a36991afcbac5faf066941ec1871be945e
Ryan Gillie <rag4214@g.rit.edu>
2020-06-07 20:29:10 -0400

    Use Typography and our fonts

--- react-app/src/pages/NotFound/NotFound.tsx
+++ react-app/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
-const NotFound: React.FC = () => <h1>404: Page Not Found</h1>;
+const NotFound: React.FC = () => <Typography variant="h1">404: Page Not Found</Typography>;
 
 export default NotFound;
