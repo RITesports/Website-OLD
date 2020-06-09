772ea3925592354f2df36d70d53489d1a36d7e87
Ryan Gillie <rag4214@g.rit.edu>
2020-06-09 16:57:19 -0400

    remove _id from sub-documents

--- express-api/src/models/team/division/division.ts
+++ express-api/src/models/team/division/division.ts
@@ -5,8 +5,6 @@ import { League, LeagueJoi } from './league';
 import { Player, PlayerJoi } from './player';
 
 export interface Division {
-  _id?: string;
-
   name?: string;
 
   players?: Player[];
@@ -18,11 +16,9 @@ export const Division = new Schema<Division>({
 
   players: [Player],
   leagues: [League],
-});
+}, { _id: false });
 
 export const DivisionJoi = Joi.object().keys({
-  _id: Joi.string().regex(/^[a-f\d]{24}$/i),
-
   name: Joi.string(),
 
   players: Joi.array().items(PlayerJoi),
