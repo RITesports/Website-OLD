a37fe9621b173465c4a68f82212f024dbee34704
Ryan Gillie <rag4214@g.rit.edu>
2020-06-09 14:22:50 -0400

    team model

---
+++ express-api/src/models/team/team.ts
@@ -0,0 +1,37 @@
+import Joi from '@hapi/joi';
+import { Document, model, Schema } from 'mongoose';
+
+import { Division, DivisionJoi } from './division';
+
+export interface Team {
+  _id?: string;
+
+  name: string;
+  identifier: string;
+
+  divisions?: Division[];
+
+  imageUrl?: string;
+}
+
+export type TeamDocument = Team & Document;
+
+export const Team = model<TeamDocument>('Team', new Schema<Team>({
+  name: { type: String, required: true },
+  identifier: { type: String, required: true, unique: true, lowercase: true },
+
+  divisions: [Division],
+
+  imageUrl: String,
+}));
+
+export const TeamJoi = Joi.object().keys({
+  _id: Joi.string().regex(/^[a-f\d]{24}$/i),
+
+  name: Joi.string().required(),
+  identifier: Joi.string().required(),
+
+  divisions: Joi.array().items(DivisionJoi),
+
+  imageUrl: Joi.string().uri(),
+});
