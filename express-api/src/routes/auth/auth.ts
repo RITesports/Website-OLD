783b2c41d3afc417e73f97965af0c431c90fbde1
Ryan Gillie <rag4214@g.rit.edu>
2020-06-09 00:07:54 -0400

    readability improvemnts

--- express-api/src/routes/auth/auth.ts
+++ express-api/src/routes/auth/auth.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 
 import googleRoutes from './google';
-import jwtConfig, { JWT } from '../../configs/jwt';
+import jwtConfig from '../../configs/jwt';
+import { User } from '../../models/user';
 
 const router = Router();
 
@@ -11,7 +12,7 @@ router.use('/google', googleRoutes);
 router.get('/isLoggedIn', (req, res) => res.status(200).json({
   status: 200,
   isLoggedIn: !!req.cookies[jwtConfig.cookieName],
-  user: req.cookies[jwtConfig.cookieName] && (jwt.decode(req.cookies[jwtConfig.cookieName]) as JWT).user,
+  user: req.cookies[jwtConfig.cookieName] && jwt.decode(req.cookies[jwtConfig.cookieName]) as User,
 }));
 
 router.get('/logout', (req, res) => {
