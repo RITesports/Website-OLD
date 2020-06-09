6e186bf5a816fbe232e8104cc490113c36642519
Ryan Gillie <rag4214@g.rit.edu>
2020-06-09 16:47:08 -0400

    remove base api routes

--- express-api/src/routes/api/api.ts
+++ express-api/src/routes/api/api.ts
@@ -6,9 +6,4 @@ const router = Router();
 
 router.use('/teams', teamsRoutes);
 
-router.get('/', (req, res) => res.status(200).json({
-  status: 200,
-  message: 'API Routes',
-}));
-
 export default router;
