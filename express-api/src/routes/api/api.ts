13a9eb7d7cc0b22d45d65f58ed12aedd4d25e7eb
Ryan Gillie <rag4214@g.rit.edu>
2020-06-09 16:46:53 -0400

    enable team routes

--- express-api/src/routes/api/api.ts
+++ express-api/src/routes/api/api.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 
+import teamsRoutes from './teams';
+
 const router = Router();
+
+router.use('/teams', teamsRoutes);
 
 router.get('/', (req, res) => res.status(200).json({
   status: 200,
