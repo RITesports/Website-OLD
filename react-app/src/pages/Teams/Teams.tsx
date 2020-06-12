1ef1ce412e80488a533ac20f73ced69e97cd21ce
Ryan Gillie <rag4214@g.rit.edu>
2020-06-12 16:33:28 -0400

    updated teams page

--- react-app/src/pages/Teams/Teams.tsx
+++ react-app/src/pages/Teams/Teams.tsx
@@ -1,6 +1,46 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
-const Teams: React.FC = () => <Typography variant="h1">Teams Page!</Typography>;
+import TeamCard from '../../components/cards/Team';
+import { useTeams } from '../../utils/team';
+
+const useStyles = makeStyles((theme) => createStyles({
+  title: {
+    margin: theme.spacing(3),
+  },
+}));
+
+const Teams: React.FC = () => {
+  const classes = useStyles();
+  const { teams, canCreate } = useTeams();
+
+  return (
+    <>
+      <Typography variant="h3" align="center" className={classes.title}>Teams</Typography>
+      <Grid container justify="center" spacing={5}>
+        {teams.map((team) => (
+          <Grid key={team._id} item>
+            <Link component={RouterLink} to={`/teams/${team.identifier}`} underline="none">
+              <TeamCard team={team} />
+            </Link>
+          </Grid>
+        ))}
+        {canCreate && (
+          <Grid item>
+            <Link component={RouterLink} to="/teams/createTeam" underline="none">
+              <TeamCard>
+                <Typography variant="h5" align="center" color="primary">Create Team</Typography>
+              </TeamCard>
+            </Link>
+          </Grid>
+        )}
+      </Grid>
+    </>
+  );
+};
 
 export default Teams;
