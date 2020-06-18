f8087b0f304a2b3ab84de696715d2a493cf4b8e4
Ryan Gillie <rag4214@g.rit.edu>
2020-06-18 00:23:18 -0400

    route TeamEdit page

--- react-app/src/Routes/Routes.tsx
+++ react-app/src/Routes/Routes.tsx
@@ -1,8 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import AdminRoute from './protected/Admin';
+import ManagerRoute from './protected/Manager';
+
 const Home = lazy(() => import('../pages/Home'));
 const Team = lazy(() => import('../pages/Team'));
+const TeamEdit = lazy(() => import('../pages/TeamEdit'));
 const Teams = lazy(() => import('../pages/Teams'));
 const NotFound = lazy(() => import('../pages/NotFound'));
 
@@ -10,17 +14,12 @@ const Routes: React.FC = () => (
   <main style={{ overflowX: 'hidden' }}>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+        <Route exact path="/"><Home /></Route>
 
-        <Route exact path="/teams">
-          <Teams />
-        </Route>
-
-        <Route exact path="/teams/:identifierOrId">
-          <Team />
-        </Route>
+        <Route exact path="/teams"><Teams /></Route>
+        <AdminRoute exact path="/teams/createTeam"><TeamEdit /></AdminRoute>
+        <Route exact path="/teams/:identifierOrId"><Team /></Route>
+        <ManagerRoute exact path="/teams/:identifierOrId/edit"><TeamEdit /></ManagerRoute>
 
         <Route>
           <NotFound />
