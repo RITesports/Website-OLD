0a2873ae697db787f2fd999a929eb00bca33fc7a
Ryan Gillie <rag4214@g.rit.edu>
2020-06-24 02:08:10 -0400

    enable match routes

--- express-api/src/routes/api/api.ts
+++ express-api/src/routes/api/api.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import matchesRoutes from './matches';
 import profilesRoutes from './profiles';
 import teamsRoutes from './teams';
 
 const router = Router();
 
+router.use('/matches', matchesRoutes);
 router.use('/profiles', profilesRoutes);
 router.use('/teams', teamsRoutes);
 
