4844d6eb6066123a0d70957ee21ac2be8c25cefa
Zach Cook <zjc9022@g.rit.edu>
2020-07-01 18:24:33 -0400

    Deleted Footer and it's references.

--- react-app/src/App/App.tsx
+++ react-app/src/App/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Routes from '../Routes';
 
 const App: React.FC = () => (
   <>
     <Header />
     <Routes />
-    <Footer />
   </>
 );
 
