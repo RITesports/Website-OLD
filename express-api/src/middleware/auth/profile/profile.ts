7ebc81bcd4e7cedc9aa8717390551bb8dfcac372
Zach Cook <zjc9022@g.rit.edu>
2020-07-06 17:52:51 -0400

    Updated Middleware.

--- express-api/src/middleware/auth/profile/profile.ts
+++ express-api/src/middleware/auth/profile/profile.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 import { RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
 
@@ -9,11 +11,11 @@ export const updateProfile: RequestHandler = (req, res, next) => {
   try {
     const user = jwt.decode(req.cookies[jwtConfig.cookieName]) as User;
 
-    if (user.role === 'Admin' || (user.profileId === req.params._id)) return next();
+    if (user.role === 'Admin' || user.profileId === req.params.id) return next();
 
     return res.status(403).json({ status: 403, message: 'Forbidden: You do not have permission to update this profile' });
   }
   catch {
-    return res.status(403).json({ status: 401, message: 'Unauthorized: You must be logged in to access this' });
+    return res.status(401).json({ status: 401, message: 'Unauthorized: You must be logged in to access this' });
   }
 };
