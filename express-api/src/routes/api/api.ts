0acc6bf7fea14034b31d3e67a61edcfe6a4b938e
Zach Cook <zjc9022@g.rit.edu>
2020-07-06 13:22:30 -0400

    Added profiles routes

--- express-api/src/routes/api/api.ts
+++ express-api/src/routes/api/api.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import teamsRoutes from './teams';
+import profilesRoutes from './profiles';
 
 const router = Router();
 
 router.use('/teams', teamsRoutes);
+router.use('/profiles', profilesRoutes);
 
 export default router;
