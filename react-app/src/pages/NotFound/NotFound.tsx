d1b57fe9e4d776919092dd16d2a06d2032cd137b
Ryan Gillie <rag4214@g.rit.edu>
2020-07-06 20:48:22 -0400

    update notfound to use neil's placeholder tiger

--- react-app/src/pages/NotFound/NotFound.tsx
+++ react-app/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
-const NotFound: React.FC = () => <Typography variant="h1">404: Page Not Found</Typography>;
+import { logoCardMeme } from '../../assets/images';
+
+const useStyles = makeStyles((theme) => createStyles({
+  title: {
+    margin: theme.spacing(3),
+  },
+  tiger: {
+    display: 'block',
+
+    height: 'auto',
+    maxWidth: '90%',
+
+    margin: 'auto',
+  },
+}));
+
+const NotFound: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Typography variant="h1" align="center" className={classes.title}>404: Page Not Found</Typography>
+      <img src={logoCardMeme} alt="Neil's Placeholder Tiger Logo" className={classes.tiger} />
+    </>
+  );
+};
 
 export default NotFound;
