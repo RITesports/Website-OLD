60f409c83464523e8d987b8b37ac87be5741d9c2
Ryan Gillie <rag4214@g.rit.edu>
2020-07-06 20:05:57 -0400

    change cursor to RitchieBongo when konami is used

--- react-app/src/App/App.tsx
+++ react-app/src/App/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Konami from 'react-konami-code';
+
+import { RitchieBongoCursor } from '../assets/images';
 
 import Header from '../components/Header';
 import Routes from '../Routes';
 
 const App: React.FC = () => (
   <>
+    <Konami action={() => {
+      document.body.style.cursor = `url(${RitchieBongoCursor}) 16 16, auto`;
+    }}
+    />
     <Header />
     <Routes />
   </>
