32d8d8fd50d3856f7d5d3358a8e545ada51ab142
Ryan Gillie <rag4214@g.rit.edu>
2020-07-08 23:57:15 -0400

    Founders Page (#93)
    * Founders page
    
    * founders menu drawer link
    
    * Founders route
    
    * Page Spacing

--- react-app/src/Routes/Routes.tsx
+++ react-app/src/Routes/Routes.tsx
@@ -7,9 +7,13 @@ import AdminRoute from './protected/Admin';
 import ManagerRoute from './protected/Manager';
 
 const Home = lazy(() => import('../pages/Home'));
+
+const Founders = lazy(() => import('../pages/Founders'));
+
 const Team = lazy(() => import('../pages/Team'));
 const TeamEdit = lazy(() => import('../pages/TeamEdit'));
 const Teams = lazy(() => import('../pages/Teams'));
+
 const NotFound = lazy(() => import('../pages/NotFound'));
 
 const Routes: React.FC = () => (
@@ -18,14 +22,14 @@ const Routes: React.FC = () => (
       <Switch>
         <Route exact path="/"><Home /></Route>
 
+        <Route exact path="/founders"><Founders /></Route>
+
         <Route exact path="/teams"><Teams /></Route>
         <AdminRoute exact path="/teams/createTeam"><TeamEdit /></AdminRoute>
         <Route exact path="/teams/:identifierOrId"><Team /></Route>
         <ManagerRoute exact path="/teams/:identifierOrId/edit"><TeamEdit /></ManagerRoute>
 
-        <Route>
-          <NotFound />
-        </Route>
+        <Route><NotFound /></Route>
       </Switch>
     </Suspense>
   </main>
