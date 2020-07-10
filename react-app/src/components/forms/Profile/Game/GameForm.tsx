c592eacb80b4a2559a84a901d37bef73b4b9ecf7
Ryan Gillie <rag4214@g.rit.edu>
2020-07-10 11:55:28 -0400

    fixed GameForm

--- react-app/src/components/forms/Profile/Game/GameForm.tsx
+++ react-app/src/components/forms/Profile/Game/GameForm.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Game from '../../../../models/profile/game';
 import { GameActions } from '../../../../utils/profile';
 
-const useStyles = makeStyles((theme) => createStyles({
-  selectField: {
-    width: '100%',
-  }
-}));
+const useStyles = makeStyles({
+  textInput: {
+    display: 'block',
+  },
+});
 
 type Props = {
   game: Game,
@@ -22,63 +20,59 @@ const GameForm: React.FC<Props> = ({ game, dispatch }) => {
   const classes = useStyles();
 
   return (
-    <Grid container alignItems="center" spacing={2}>
-      <Grid item xs={3}>
-        <TextField
-          required
-          value={game.name || ''}
-          error={!game.name}
-          helperText={!game.name && 'Game Title Required'}
-          label="Game Title"
-          variant="outlined"
-          margin="normal"
-          onChange={(e) => dispatch({ type: 'GAME_SET_NAME', game: game, name: e.target.value })}
-          fullWidth
-        />
-      </Grid>
-      <Grid item xs={3}>
-        <TextField
-          required
-          value={game.platform || ''}
-          error={!game.platform}
-          helperText={!game.platform && 'Platform Required'}
-          id="select"
-          label="Platform"
-          variant="outlined"
-          margin="normal"
-          onChange={(e) => dispatch({ type: 'GAME_SET_PLATFORM', game, platform: e.target.value as "PC" | "PlayStation" | "Xbox" | "Switch" | "Other" })}
-          select
-          fullWidth
-        >
-          {["PC", "PlayStation", "Xbox", "Switch", "Other"].map((platform) => (
-            <MenuItem key={platform} value={platform}>{platform}</MenuItem>
-          ))}
-        </TextField>
-      </Grid>
-      <Grid item xs={3}>
-        <TextField
-          required
-          value={game.username || ''}
-          error={!game.username}
-          helperText={!game.username && 'Username Required'}
-          label="Username"
-          variant="outlined"
-          margin="normal"
-          onChange={(e) => dispatch({ type: 'GAME_SET_USERNAME', game: game, username: e.target.value })}
-          fullWidth
-        />
-      </Grid>
-      <Grid item xs={3}>
-        <TextField
-          value={game.tracker || ''}
-          label="Statistics Tracker Url"
-          variant="outlined"
-          margin="normal"
-          onChange={(e) => dispatch({ type: 'GAME_SET_TRACKER', game: game, tracker: e.target.value })}
-          fullWidth
-        />
-      </Grid>
-    </Grid>
+    <>
+      <TextField
+        required
+        value={game.name || ''}
+        error={!game.name}
+        helperText={!game.name && 'Game Title Required'}
+        label="Game Title"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        onChange={(e) => dispatch({ type: 'GAME_SET_NAME', game, name: e.target.value })}
+        className={classes.textInput}
+      />
+      <TextField
+        select
+        required
+        value={game.platform || ''}
+        error={!game.platform}
+        helperText={!game.platform && 'Platform Required'}
+        id="select"
+        label="Platform"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        onChange={(e) => dispatch({ type: 'GAME_SET_PLATFORM', game, platform: e.target.value as 'PC' | 'PlayStation' | 'Xbox' | 'Switch' | 'Other' })}
+        className={classes.textInput}
+      >
+        {['PC', 'PlayStation', 'Xbox', 'Switch', 'Other'].map((platform) => (
+          <MenuItem key={platform} value={platform}>{platform}</MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        required
+        value={game.username || ''}
+        error={!game.username}
+        helperText={!game.username && 'Username Required'}
+        label="Username"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        onChange={(e) => dispatch({ type: 'GAME_SET_USERNAME', game, username: e.target.value })}
+        className={classes.textInput}
+      />
+      <TextField
+        value={game.tracker || ''}
+        label="Statistics Tracker URL"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        onChange={(e) => dispatch({ type: 'GAME_SET_TRACKER', game, tracker: e.target.value })}
+        className={classes.textInput}
+      />
+    </>
   );
 };
 
