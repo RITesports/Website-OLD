7ce295805dec57e527b8399304e465851d78fba2
Ryan Gillie <rag4214@g.rit.edu>
2020-08-04 03:06:25 -0400

    Add merch assets + update banner

--- react-app/src/assets/assets.ts
+++ react-app/src/assets/assets.ts
@@ -1,10 +1,13 @@
 // Snake case for assets
 /* eslint-disable @typescript-eslint/naming-convention */
 
-export const Banner_Home = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Banner_Home.jpg';
+export const Banner_Home = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Banner_Home.png';
 export const Banner_Profile = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Banner_Profile.jpg';
+export const Banner_Welcome_Back = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Banner_Welcome_Back.png';
 export const BongoRitchie_Cursor = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/BongoRitchie_Cursor.png';
 export const Kitch_Holding_Keyboard = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Kitch_Holding_Keyboard.jpg';
+export const Merch_Casual = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Merch_Casual.png';
+export const Merch_Sports = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Merch_Sports.png';
 export const Wordmark_Long_Gray = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Long_Gray.svg';
 export const Wordmark_Long_White = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Long_White.svg';
 export const Wordmark_Stacked_Gray = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Stacked_Gray.svg';
