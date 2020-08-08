4f3685207cf6830561c2f4574fd95c9d7c3027c9
Ryan Gillie <rag4214@g.rit.edu>
2020-08-08 17:50:31 -0400

    new teams page with TABS

--- react-app/src/pages/Teams/Teams.tsx
+++ react-app/src/pages/Teams/Teams.tsx
@@ -1,53 +1,58 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useParams } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 
 import Alert from '@material-ui/lab/Alert';
 
-import TeamCard from '../../components/cards/Team';
-import { useTeams } from '../../utils/team';
+import { Controller } from '../../assets';
+import TeamDetails from '../../components/details/Team';
+import { FromLink } from '../../icons';
+import { useTeams, useTeam } from '../../utils/team';
 
-const useStyles = makeStyles((theme) => createStyles({
-  title: {
-    margin: theme.spacing(3),
-  },
-}));
-
+type Params = {
+  identifierOrId?: string;
+};
 const Teams: React.FC = () => {
-  const classes = useStyles();
-  const { teams, error, canCreate } = useTeams();
+  const { identifierOrId } = useParams<Params>();
+
+  const { teams, error: teamsError, canCreate } = useTeams();
+  const { team, error: teamError, canEdit } = useTeam(identifierOrId || teams[0]?.identifier);
 
   return (
-    <>
-      {error
-        ? <Alert severity="error">{error}</Alert>
-        : (
-          <>
-            <Typography variant="h3" align="center" className={classes.title}>Teams</Typography>
-            <Grid container justify="center" spacing={5}>
-              {teams.map((team) => (
-                <Grid key={team._id} item>
-                  <Link component={RouterLink} to={`/teams/${team.identifier}`}>
-                    <TeamCard team={team} />
-                  </Link>
+    teamsError
+      ? <Alert severity="error">{teamsError}</Alert>
+      : (
+        <>
+          <Tabs value={team.identifier || false} variant="scrollable" scrollButtons="on" indicatorColor="primary">
+            {teams.map((tabTeam) => (
+              <Tab value={tabTeam.identifier} icon={<FromLink src={tabTeam.imageUrl || Controller} alt={team.imageUrl ? `${team.name} Icon` : 'Controller Icon'} />} component={RouterLink} to={`/teams/${tabTeam.identifier}`} key={tabTeam._id} />
+            ))}
+            {canCreate && (
+              <Tab label="Create Team" icon={<FromLink src={Controller} alt="Controller Icon" />} component={RouterLink} to="/teams/createTeam" />
+            )}
+          </Tabs>
+          {teamError
+            ? <Alert severity="error">{teamError}</Alert>
+            : (
+              <Container disableGutters>
+                <Grid container direction="column" alignItems="center" spacing={3}>
+                  <Grid item>
+                    <TeamDetails team={team} />
+                  </Grid>
+                  {canEdit && (
+                    <Grid item>
+                      <Button variant="contained" color="primary" component={RouterLink} to={`/teams/${team.identifier}/edit`}>Edit Team</Button>
+                    </Grid>
+                  )}
                 </Grid>
-              ))}
-              {canCreate && (
-                <Grid item>
-                  <Link component={RouterLink} to="/teams/createTeam" underline="none">
-                    <TeamCard>
-                      <Typography variant="h5" align="center" color="primary">Create Team</Typography>
-                    </TeamCard>
-                  </Link>
-                </Grid>
-              )}
-            </Grid>
-          </>
-        )}
-    </>
+              </Container>
+            )}
+        </>
+      )
   );
 };
 
