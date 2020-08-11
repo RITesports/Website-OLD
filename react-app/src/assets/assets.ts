a5dedd40ecac15a82aaa96a847ed29ce73268d84
Ryan Gillie <rag4214@g.rit.edu>
2020-08-11 03:41:15 -0400

    new wallpaper img

--- react-app/src/assets/assets.ts
+++ react-app/src/assets/assets.ts
@@ -8,11 +8,12 @@ export const BongoRitchie_Cursor = 'https://hooli-drive.sfo2.digitaloceanspaces.
 export const Kitch_Holding_Keyboard = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Kitch_Holding_Keyboard.jpg';
 export const Merch_Casual = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Merch_Casual.png';
 export const Merch_Sports = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Merch_Sports.png';
+export const TigerLogo_Meme = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/TigerLogo_Meme.png';
+export const Wallpaper = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wallpaper.png';
 export const Wordmark_Long_Gray = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Long_Gray.svg';
 export const Wordmark_Long_White = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Long_White.svg';
 export const Wordmark_Stacked_Gray = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Stacked_Gray.svg';
 export const Wordmark_Stacked_White = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/Wordmark_Stacked_White.svg';
-export const TigerLogo_Meme = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/assets/TigerLogo_Meme.png';
 
 export const Controller = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/teams/Controller.svg';
 export const Member_No_Photo = 'https://hooli-drive.sfo2.digitaloceanspaces.com/website/public/members/Member_No_Photo.png';
