08df58f854bc989acda9552d0cb8eb16117a7b24
zjc9022 <zjc9022@g.rit.edu>
2020-08-10 21:26:00 -0400

    Switched package, moved button to Profile page only if it is the user's profile.

--- react-app/src/pages/ProfileEdit/ProfileEdit.tsx
+++ react-app/src/pages/ProfileEdit/ProfileEdit.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
-import copy from 'clipboard-copy';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -59,7 +58,6 @@ const ProfileEdit: React.FC = () => {
       <Grid container alignItems="center" className={classes.banner}>
         <Grid item className={classes.title}>
           <Typography variant="h3">User Profile</Typography>
-          <Button variant="contained" color="primary" onClick={() => copy(profile._id)}>Copy Profile ID</Button>
         </Grid>
       </Grid>
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
