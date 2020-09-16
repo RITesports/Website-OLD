3e36b5287ca9156e4cf5041a1dab268050a4dca2
Zach Cook <zjc9022@g.rit.edu>
2020-09-16 16:33:18 -0400

    Mapped awards on the the profileCard.

--- react-app/src/components/cards/Profile/ProfileCard.tsx
+++ react-app/src/components/cards/Profile/ProfileCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Chip from '@material-ui/core/Chip';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
@@ -19,6 +20,11 @@ const useStyles = makeStyles((theme) => createStyles({
   },
   bio: {
     marginTop: theme.spacing(1),
+  },
+  chip: {
+    marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(.5),
+    borderWidth: '2px',
   },
 }));
 
@@ -43,6 +49,9 @@ const ProfileCard: React.FC<Props> = ({ profile, children }) => {
             <Typography variant="h5" align="center" className={classes.username}>{profile.name || 'About'}</Typography>
             <Divider />
             <Typography align="center" className={classes.bio}>{profile.bio || 'No Profile Bio'}</Typography>
+            {profile.chips?.map((chip) => (
+              <Chip label={chip.text} style={{ borderColor: chip.color }} variant="outlined" className={classes.chip} />
+            ))}
           </>
         ))}
       </CardContent>
